745f6c596927945524c0f10489c1a28626760742
Young <Young@lanou3gdemac-mini-3.local>
2016-10-08 14:52:04 +0800

    Initial Commit

---
+++ 自定义TabBar/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  自定义TabBar
+//
+//  Created by Young on 10/8/16.
+//  Copyright © 2016 Young. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
